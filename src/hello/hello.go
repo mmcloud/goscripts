dbffb05583c9a9a9894d4a09659b07eda72dd71e
Matt McLeod <mtthwmcld@gmail.com>
2020-10-08 16:12:44 +0100

    hello: dependency injection

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,6 +1,12 @@
-package hello
+package main
 
-import quoteV3 "rsc.io/quote/v3"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	quoteV3 "rsc.io/quote/v3"
+)
 
 func Hello() string {
 	return quoteV3.HelloV3()
@@ -8,4 +14,18 @@ func Hello() string {
 
 func Proverb() string {
 	return quoteV3.Concurrency()
+}
+
+func Greet(writer io.Writer, name string) {
+	// fmt.Printf("Hello, %s", name)
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+// send through http writer
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
